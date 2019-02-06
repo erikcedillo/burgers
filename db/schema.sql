11c9e9f02907445666b8cbec1ed82835b2c86f5e
Erik Cedillo <cedilloerik66@outlook.com>
2019-02-05 16:45:14 -0800

    somewhat fuctional

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
+    DROP DATABASE IF EXISTS burgers_db;
     CREATE DATABASE burgers_db;
     USE burgers_db;
 
 -- Created the table "schools" 
 CREATE TABLE burgers
 (
-  id int AUTO_INCREMENT,
-  burgers_name varchar (150) NOT NULL,
-  devoured BOOLEAN,
-  PRIMARY KEY (id)
+  id int NOT NULL AUTO_INCREMENT,
+    burgers_name  varchar(150) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
